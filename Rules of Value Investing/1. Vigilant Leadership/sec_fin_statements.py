89beb0e32f580295f247ecda30728a19a6bb07c0
lt47 <55928190+lt47@users.noreply.github.com>
2023-03-30 15:20:51 -0500

    getting more items from txt and xbrl versions of fin statements.

--- Rules of Value Investing/1. Vigilant Leadership/sec_fin_statements.py
+++ Rules of Value Investing/1. Vigilant Leadership/sec_fin_statements.py
@@ -57,9 +57,25 @@ class SecFinData():
             resp_period = datetime.strptime(
                 instjson['facts'][f'f{j}']['dimensions']['period'].split('/')[0], '%Y-%m-%d')
             # need to import datetime module, convert date strings and find out if period is within 3 months of filing date
-            if instjson['facts'][f'f{j}']['dimensions']['concept'] == 'Liabilities' and abs(filing_date-resp_period) <= timedelta(days=90):
-                single_statement_response.update(
-                    {'totalLiabilities': instjson['facts'][f'f{j}']['value'], 'filing_period': instjson['facts'][f'f{j}']['dimensions']['period']})
+            if instjson['facts'][f'f{j}']['dimensions']['concept'] == 'Liabilities' and abs(filing_date-resp_period) <= timedelta(days=120):
+                total_liabilities = instjson['facts'][f'f{j}']['value']
+            elif instjson['facts'][f'f{j}']['dimensions']['concept'] == 'StockholdersEquityIncludingPortionAttributableToNoncontrollingInterest' and 'StatementEquityComponentsAxis' not in instjson['facts'][f'f{j}']['dimensions'] and abs(filing_date-resp_period) <= timedelta(days=90):
+                total_shareholders_equity = instjson['facts'][f'f{j}']['value']
+            elif instjson['facts'][f'f{j}']['dimensions']['concept'] == 'AssetsCurrent' and abs(filing_date-resp_period) <= timedelta(days=120):
+                total_current_assets = instjson['facts'][f'f{j}']['value']
+            elif instjson['facts'][f'f{j}']['dimensions']['concept'] == 'LiabilitiesCurrent' and abs(filing_date-resp_period) <= timedelta(days=120):
+                total_current_liabilities = instjson['facts'][f'f{j}']['value']
+            elif instjson['facts'][f'f{j}']['dimensions']['concept'] == 'NetIncomeLoss' and abs(filing_date-resp_period) <= timedelta(days=120):
+                net_income_loss = instjson['facts'][f'f{j}']['value']
+
+        debt_to_equity = round(
+            float(total_liabilities/total_shareholders_equity), 2)
+        current_ratio = round(
+            float(total_current_assets/total_current_liabilities), 2)
+        roe = round(
+            float(net_income_loss/total_shareholders_equity), 2)
+        single_statement_response.update(
+            {'totalLiabilities': total_liabilities, 'totalShareholdersEquity': total_shareholders_equity, 'debtToEquity': debt_to_equity, 'totalCurrentAssets': total_current_assets, 'totalCurrentLiabilities': total_current_liabilities, 'currentRatio': current_ratio, 'netIncomeOrLoss': net_income_loss, 'returnOnEquity': roe})
 
         return single_statement_response
 
@@ -77,17 +93,76 @@ class SecFinData():
         }
         data = requests.get(schema_url, headers=headers).text
 
-        currency_units = ['thousands', 'millions', 'billions']
+        currency_units = {'thousands': 1000,
+                          'millions': 1000000, 'billions': 1000000000}
+        total_liabilities = ''
+        total_shareholders_equity = ''
+        total_current_assets = ''
+        total_current_liabilities = ''
+        pref_stock_dividends = ''
+        net_income_loss_commonshares = ''
+        net_income_loss = ''
         for index, line in enumerate(data.split('\n')):
+            line = line.lstrip()
+            # total_liab_check = re.findall(r'(Total [l|L][a-zA-Z\s]*\b)', line)
+            total_liab_check = re.findall(
+                r'^(Total Liabilities\b)', line, re.IGNORECASE)
+            total_sheq_check = re.findall(
+                r"^(Total [s|S][a-zA-Z\s']*\b)", line)
+            total_currass_check = re.findall(
+                r"^(Total Current Assets\b)", line, re.IGNORECASE)
+            total_currliab_check = re.findall(
+                r"^(Total Current Liabilities\b)", line, re.IGNORECASE)
+            pref_stockdiv_check = re.findall(
+                r"^(Preferred stock dividends\b)", line, re.IGNORECASE)
+            net_incomelosscomm_check = re.findall(
+                r"^(common shareholders\b)", line, re.IGNORECASE)
             if 'Dollars in ' in line:
                 currency_unit = re.findall(
                     r'Dollars in.*[n|d]s\b', line)[0].replace(')', '').split()[2].lower()
-            if 'Total liabilities' in line or 'Total Liabilities' in line:
-                # convert the total liabilities amount to an integer and convert it to currency unit
+            elif len(total_liab_check) > 0 and total_liab_check[0].rstrip().lower() in ('total liabilities'):
                 total_liabilities = re.findall(
-                    r'\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)', line)[0]
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                total_liabilities = f"-{total_liabilities.replace('(','').replace(')','')}" if '(' in total_liabilities else total_liabilities
+                total_liabilities = int(total_liabilities.replace(
+                    ',', '')) * currency_units[currency_unit]
+            elif len(total_sheq_check) > 0 and total_sheq_check[0].rstrip().lower() in ("total stockholders' equity", "total shareholders' equity"):
+                total_shareholders_equity = re.findall(
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                total_shareholders_equity = f"-{total_shareholders_equity.replace('(','').replace(')','')}" if '(' in total_shareholders_equity else total_shareholders_equity
+                total_shareholders_equity = int(total_shareholders_equity.replace(
+                    ',', '')) * currency_units[currency_unit]
+            elif len(total_currass_check) > 0 and total_currass_check[0].rstrip().lower() in ("total current assets"):
+                total_current_assets = re.findall(
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                total_current_assets = f"-{total_current_assets.replace('(','').replace(')','')}" if '(' in total_current_assets else total_current_assets
+                total_current_assets = int(total_current_assets.replace(
+                    ',', '')) * currency_units[currency_unit]
+            elif len(total_currliab_check) > 0 and total_currliab_check[0].rstrip().lower() in ("total current liabilities"):
+                total_current_liabilities = re.findall(
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                total_current_liabilities = f"-{total_current_liabilities.replace('(','').replace(')','')}" if '(' in total_current_liabilities else total_current_liabilities
+                total_current_liabilities = int(total_current_liabilities.replace(
+                    ',', '')) * currency_units[currency_unit]
+            elif len(pref_stockdiv_check) > 0 and pref_stockdiv_check[0].rstrip().lower() in ("preferred stock dividends"):
+                pref_stock_dividends = re.findall(
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                pref_stock_dividends = f"-{pref_stock_dividends.replace('(','').replace(')','')}" if '(' in pref_stock_dividends else pref_stock_dividends
+            elif len(net_incomelosscomm_check) > 0 and net_incomelosscomm_check[0].rstrip().lower() in ("common shareholders"):
+                net_income_loss_commonshares = re.findall(
+                    r'\(?\b\d{1,3}(?:,\d{3})*(?:\.\d+)?(?!\d)\)?', line)[0]
+                net_income_loss_commonshares = f"-{net_income_loss_commonshares.replace('(','').replace(')','')}" if '(' in net_income_loss_commonshares else net_income_loss_commonshares
 
-                pass
+        net_income_loss = (int(pref_stock_dividends.replace(',', '')) + int(
+            net_income_loss_commonshares.replace(',', ''))) * currency_units[currency_unit]
+        debt_to_equity = round(
+            float(total_liabilities/total_shareholders_equity), 2)
+        current_ratio = round(
+            float(total_current_assets/total_current_liabilities), 2)
+        roe = round(
+            float(net_income_loss/total_shareholders_equity), 2)
+        single_statement_response.update(
+            {'totalLiabilities': total_liabilities, 'totalShareholdersEquity': total_shareholders_equity, 'debtToEquity': debt_to_equity, 'totalCurrentAssets': total_current_assets, 'totalCurrentLiabilities': total_current_liabilities, 'currentRatio': current_ratio, 'netIncomeOrLoss': net_income_loss, 'returnOnEquity': roe})
 
         return single_statement_response
 
